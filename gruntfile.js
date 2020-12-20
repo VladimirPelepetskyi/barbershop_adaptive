0dbb0b42092bcdfc71abc2a7110ddc654d08adc7
VladimirPelepetskyi <vladimirpelepetskiy@gmail.com>
2020-12-20 21:05:31 +0200

    Добавил фото формата webp

--- gruntfile.js
+++ gruntfile.js
@@ -1,40 +1,65 @@
-"use strict";
-
 module.exports = function (grunt) {
-	//	grunt.loadNpmTasks("grunt-contrib-less");
-	//	grunt.loadNpmTasks("grunt-browser-sync");
-	//	grunt.loadNpmTasks("grunt-contrib-watch");
-	//	grunt.loadNpmTasks("grunt-postcss");
 
 	require("load-grunt-tasks")(grunt);
+	require('time-grunt')(grunt);
+
 
 	grunt.initConfig({
 		less: {
 			style: {
 				files: {
-					"css/style.css": "less/style.less"
+					'css/style.css': 'less/style.less'
 				}
 			}
 		},
-
-		//		postcss: {
-		//			style: {
-		//				options: {
-		//					processors: [
-		//						require("autoprefixer")()
-		//					]
-		//				},
-		//				src: "css/*.css"
-		//			}
-		//		},
-
-		watch: {
+		postcss: {
 			style: {
-				files: ["less/**/*.less"],
-				tasks: ["less"]
+				options: {
+					processors: [
+						require('autoprefixer')()
+					]
+				},
+				src: "css/*.css"
+
 			}
 		},
-
+		csso: {
+			style: {
+				options: {
+					report: "gzip"
+				},
+				files: {
+					"css/style.min.css": ["css/style.css"],
+					"css/normalize.min.css": ["css/normalize.css"]
+				}
+			}
+		},
+		imagemin: {
+			images: {
+				options: {
+					optimizationLevel: 3,
+					svgoPlugins: [{
+						removeViewBox: false
+					}],
+					progressive: true
+				},
+				files: [{
+					expand: true,
+					src: ["img/**/*.{png,jpg,svg}"]
+				}]
+			}
+		},
+		cwebp: {
+			images: {
+				options: {
+					q: 90
+				},
+				files: [{
+					expand: true,
+					src: ["img/**/*.{png,jpg}"]
+				}]
+			}
+		},
 		browserSync: {
 			server: {
 				bsFiles: {
@@ -50,7 +75,19 @@ module.exports = function (grunt) {
 				}
 			}
 		},
+		watch: {
+			style: {
+				files: ["less/**/*.less"],
+				tasks: ["less", "postcss", "csso"]
+			}
+		}
 	});
 
 	grunt.registerTask("serve", ["browserSync", "watch"]);
+
+	grunt.registerTask("build", [
+		"less",
+		"postcss",
+		"csso"
+	]);
 };
